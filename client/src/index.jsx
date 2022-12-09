678309c7891771b6fd4606465098d62fccfb6184
Yaser Eisa <yasereisa@Yasers-MacBook-Air.local>
2022-12-08 22:12:37 -0500

    App.jsx created

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,17 +1,8 @@
-import React from "react";
-import { render } from "react-dom";
-import OverView from './components/overview/index.jsx'
-import QuestionAnswers from './components/q&a/index.jsx'
-import RatingsReviews from './components/ratings&reviews/index.jsx'
-import RelatedItemsComp from './components/relatedItems&comp/index.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx'
 
-render(
-  <div>
-    <h1>Front End Capstone</h1>
-    <OverView/>
-    <RelatedItemsComp/>
-    <QuestionAnswers/>
-    <RatingsReviews/>
-  </div>,
-  document.getElementById("root")
-);
+
+
+
+ReactDOM.render(<App />, document.getElementById("root"));
