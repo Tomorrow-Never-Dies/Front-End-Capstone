68f3bd3fffa5ddb08bd61e7b1a45a753496e73bd
Aman Arabi <amanarabi@gmail.com>
2022-12-23 16:55:27 -0600

    added styles

--- client/src/components/overview/index.jsx
+++ client/src/components/overview/index.jsx
@@ -4,6 +4,7 @@ import sampleProductIdStyles from '../../../../fixtures/Overview/IdProducts.js';
 import sampleProductId from '../../../../fixtures/Overview/ProductsID.js';
 import sampleData from '../../../../fixtures/ratings&reviews/ReviewExampleData.js'
 import $ from 'jquery';
+import './look.css';
 
 export default class OverView extends React.Component {
   constructor (props) {
@@ -17,7 +18,7 @@ export default class OverView extends React.Component {
       selectedSize: {},
       starToggled: false
     };
-   // this.initialRender = this.initialRender.bind(this);
+    // this.initialRender = this.initialRender.bind(this);
     this.starToggle = this.starToggle.bind(this);
     this.addToCart = this.addToCart.bind(this);
     this.getproducts = this.getproducts.bind(this);
@@ -31,7 +32,7 @@ export default class OverView extends React.Component {
   // }
 
   starToggle () {
-    //console.log(this.state.starToggled, 'togggleeee')
+    // console.log(this.state.starToggled, 'togggleeee')
 
     this.setState({ starToggled: !this.state.starToggled }) // need to communitcate to yassir
   }
@@ -42,10 +43,9 @@ export default class OverView extends React.Component {
       contentType: 'application/json',
       context: this,
       url: `/products/${this.props.id}`,
-      data: {id: this.props.id},
+      data: { id: this.props.id },
       success: (data) => {
-
-        //console.log(data, 'dataaaaaa22')
+        // console.log(data, 'dataaaaaa22')
         this.setState({ product: data[0] });
       },
       error: (error) => {
@@ -58,19 +58,16 @@ export default class OverView extends React.Component {
       context: this,
       url: `/products/${this.props.id}/styles`,
       data: {
-        id: this.props.id,
+        id: this.props.id
       },
       success: (data) => {
-
-        //console.log(data, 'data from stylesssss')
+        console.log(data.results, 'data from stylesssss333')
         this.setState({ styles: data.results, selectedStyle: data.results[0] });
       },
       error: (error) => {
         console.log(error, 'error geting data in ajaxxxx');
       }
     });
-
-
 
     // $.ajax({
     //   type: 'GET',
@@ -92,9 +89,16 @@ export default class OverView extends React.Component {
     console.log('added to cart!')
   }
 
+  changeSelectedStyle (event) {
+    console.log('selected style changed');
+    console.log(event.target, 'eeeeeee')
+    // this.setState({selectedStyle: });
+    // also need to add checkmark
+  }
+
   componentDidMount () { this.getproducts() }
   render () {
-    //console.log(this.state.product, 'productssssss')
+    // console.log(this.state.product, 'productssssss')
 
     if (this.state.styles === undefined || this.state.product === undefined) {
       return (
@@ -106,26 +110,30 @@ export default class OverView extends React.Component {
       return (
       <div>
        <p> OverView Place Holder </p>
+       <div className = "gallery">
+       {this.state.selectedStyle.photos !== {}
+         ? <img className = "mainImg" src={this.state.selectedStyle.photos[0].url} />
+         : <div>  Please Wait while we load our products... </div> }
+
+        {this.state.selectedStyle.photos.map(image => (
+          <img className="selectedStyleImages" src={image.thumbnail_url} />
+        )
+        )}
+       </div>
        <div data-testid='name header'>NAME: {this.state.product.name}</div>
-       <div>PRICE: {this.state.selectedStyle.original_price}</div>
+       <div>PRICE: ${this.state.selectedStyle.original_price}</div>
        <div>CATEGORY: {this.state.product.category}</div>
        <div>TITLE:{this.state.product.slogan} </div>
        <div>DESCRIPTION: {this.state.product.description} </div>
        <div>STAR RATINGS: </div>
        <a className="skip-link" href="#Reviews">Read all {this.state.reviews.length} reviews</a>
-       <div></div>
-       {/* Still need to fix href link to reviews component */}
        <button onClick = {this.starToggle}>Star toggle</button>
-       <div></div>
-       {this.state.selectedStyle.photos !== {}
-         ? <img src={this.state.selectedStyle.photos[0].url} alt="Selected style image" width="500" height="600"/>
-         : <div>  Please Wait while we load our products... </div> }
-
        {this.state.styles.map(style => (<div key = {style.style_id}>
 
-        <img src={style.photos[0].thumbnail_url} alt="style thumbNail" width="500" height="600"/>
+        <img className="styleList" src={style.photos[0].thumbnail_url} onClick = {this.changeSelectedStyle} />
           </div>)
        )}
+
         <form>
         <b> Select your Size </b>
         <select id = "mySize" >
